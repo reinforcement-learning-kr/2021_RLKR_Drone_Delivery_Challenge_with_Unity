70e2f503c5fe4c60b7c3c4dfaacd359b0186a7ed
Young_Painter_L <45022536+LeejwUniverse@users.noreply.github.com>
2021-11-01 20:05:17 +0900

    Update Agent_user.py

--- baseline/code/Agent_user.py
+++ baseline/code/Agent_user.py
@@ -22,7 +22,7 @@ class Agent:
             3 continous actions vector (range -1 ~ 1) from policy.
             ex) [-1~1, -1~1, -1~1]
         """
-        action = np.random.uniform(-1.0, 1.0, 3) # Random policy for evaluation.
+        action = np.random.uniform(-1.0, 1.0, 3) # example random action.
 
         return action
     
